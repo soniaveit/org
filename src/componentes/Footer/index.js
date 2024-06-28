ba7abb35a401dbb6e8c304248965c9a55557a8af
Sonia <soniaveit.moron@gmail.com>
2024-06-28 00:43:45 -0300

    No se ve el footer, agregué un punto al comienzo del path

--- src/componentes/Footer/index.js
+++ src/componentes/Footer/index.js
@@ -4,16 +4,16 @@ const Footer = () => {
     return <footer className="footer" style={{backgroundImage:"url(/img/Footer.png)"}}>
     <div className="redes">
     <a href='https://www.aluracursos.com/'> 
-        <img src="/img/facebook.png" alt="Facebook"/>
+        <img src="./img/facebook.png" alt="Facebook"/>
     </a>
     <a href='https://www.aluracursos.com/'> 
-        <img src="/img/twitter.png" alt="twitter" />
+        <img src="./img/twitter.png" alt="twitter" />
     </a>
     <a href='https://www.aluracursos.com/'> 
-        <img src="/img/instagram.png" alt="instagram" />
+        <img src="./img/instagram.png" alt="instagram" />
     </a>
     </div>
-    <img src='/img/Logo.png' alt='org' /> 
+    <img src='./img/Logo.png' alt='org' /> 
     <strong>Desarrollado por Sonia Veit</strong>
 </footer>
 
