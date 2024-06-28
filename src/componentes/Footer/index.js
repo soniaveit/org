03129919d565195b422af766d5f0248ef9d0893f
Sonia <soniaveit.moron@gmail.com>
2024-06-28 01:48:18 -0300

    Modificado el path de la imagen de footer

--- src/componentes/Footer/index.js
+++ src/componentes/Footer/index.js
@@ -1,19 +1,19 @@
 import "./Footer.css"
 
 const Footer = () => {
-    return <footer className="footer" style={{backgroundImage:"url(/img/Footer.png)"}}>
+    return <footer className="footer" style={{backgroundImage:"url(/img/footer.png)"}}>
     <div className="redes">
     <a href='https://www.aluracursos.com/'> 
-        <img src="./img/facebook.png" alt="Facebook"/>
+        <img src="/img/facebook.png" alt="Facebook"/>
     </a>
     <a href='https://www.aluracursos.com/'> 
-        <img src="./img/twitter.png" alt="twitter" />
+        <img src="/img/twitter.png" alt="twitter" />
     </a>
     <a href='https://www.aluracursos.com/'> 
-        <img src="./img/instagram.png" alt="instagram" />
+        <img src="/img/instagram.png" alt="instagram" />
     </a>
     </div>
-    <img src='./img/Logo.png' alt='org' /> 
+    <img src='/img/Logo.png' alt='org' /> 
     <strong>Desarrollado por Sonia Veit</strong>
 </footer>
 
